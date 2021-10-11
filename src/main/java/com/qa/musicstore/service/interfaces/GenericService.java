d5d9cda18910ce8db5c9e1d7055b3bf2801b62d2
Matthew-Place <matt.1504@hotmail.co.uk>
2021-10-11 14:21:40 +0100

    Store services generated with create method.
    Generic services hold the shared methods such as create etc.

---
+++ src/main/java/com/qa/musicstore/service/interfaces/GenericService.java
@@ -0,0 +1,12 @@
+package com.qa.musicstore.service.interfaces;
+
+import java.util.List;
+
+public interface GenericService<T, K extends Object> {
+
+	public T mapToDTO(K k);
+
+	public List<T> mapToDTO(List<K> k);
+
+	public T create(K k);
+}
