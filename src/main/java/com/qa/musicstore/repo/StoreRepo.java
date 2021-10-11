140867c21d4424c0b065eb9bdbd2200ef231bf19
Matthew-Place <matt.1504@hotmail.co.uk>
2021-10-11 14:09:09 +0100

    MS1-6 #comment @Repository added

--- src/main/java/com/qa/musicstore/repo/StoreRepo.java
+++ src/main/java/com/qa/musicstore/repo/StoreRepo.java
@@ -1,9 +1,11 @@
 package com.qa.musicstore.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.qa.musicstore.data.Store;
 
+@Repository
 public interface StoreRepo extends JpaRepository<Store, Integer> {
 
 }
