6eaf35b7c9353725b391da4969faea0b302c1b20
Matthew-Place <matt.1504@hotmail.co.uk>
2021-10-11 12:16:36 +0100

    MS1-1 Created Spring project

---
+++ src/test/java/com/qa/musicstore/MusicStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.musicstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
