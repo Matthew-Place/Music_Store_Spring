c863b4addd2549dd372077e184b2d341553a9164
Matthew-Place <matt.1504@hotmail.co.uk>
2021-10-11 16:59:18 +0100

    [MSI-5] Item services created

---
+++ src/main/java/com/qa/musicstore/service/interfaces/ItemService.java
@@ -0,0 +1,8 @@
+package com.qa.musicstore.service.interfaces;
+
+import com.qa.musicstore.data.Item;
+import com.qa.musicstore.dto.ItemDTO;
+
+public interface ItemService extends GenericService<ItemDTO, Item>{
+
+}
