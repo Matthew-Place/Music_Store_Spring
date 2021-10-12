ccb2271252ce75dd9cd3d3a30ab6a390079b8547
Matthew-Place <matt.1504@hotmail.co.uk>
2021-10-12 11:00:33 +0100

    [MS1-4] Store integration test for create.
    Implemented and working.

---
+++ src/test/java/com/qa/musicstore/controller/StoreIntegrationTest.java
@@ -0,0 +1,51 @@
+package com.qa.musicstore.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.musicstore.data.Store;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc // sets up the MockMVC object
+@Sql(scripts = { "classpath:tables/store-schema.sql",
+		"classpath:tables/store-data.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test")
+class StoreIntegrationTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	private ObjectMapper mapper;
+
+	@Test
+	void testCreate() throws Exception {
+		final Store testStore = new Store(null, "Me", "Home", "000000000000");
+		String testStoreJSON = mapper.writeValueAsString(testStore);
+
+		final Store savedStore = new Store(2, "Me", "Home", "000000000000");
+		String savedStoreJSON = mapper.writeValueAsString(savedStore);
+
+		RequestBuilder request = post("/Store/create").contentType(MediaType.APPLICATION_JSON).content(testStoreJSON);
+
+		ResultMatcher checkStatus = status().isCreated();
+		ResultMatcher checkContent = content().json(savedStoreJSON);
+
+		mvc.perform(request).andExpect(checkStatus).andExpect(checkContent);
+	}
+}
