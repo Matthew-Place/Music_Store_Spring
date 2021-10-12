40c3391699f1622dfcc9b43e0f77ad2d40a03aef
Matthew-Place <matt.1504@hotmail.co.uk>
2021-10-12 21:52:14 +0100

    [MS1-4] Updated store testing to include item list

--- src/test/java/com/qa/musicstore/service/StoreServiceUnitTest.java
+++ src/test/java/com/qa/musicstore/service/StoreServiceUnitTest.java
@@ -14,13 +14,23 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.qa.musicstore.data.Item;
 import com.qa.musicstore.data.Store;
+import com.qa.musicstore.dto.ItemDTO;
 import com.qa.musicstore.dto.StoreDTO;
 import com.qa.musicstore.repo.StoreRepo;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class StoreServiceUnitTest {
+	final Item item = new Item(1, "Instrument", "String", "Guitar", "Fender", "Classic", 1000, 10, null);
+	final List<Item> items = new ArrayList<>(Arrays.asList(item));
+	final Store store = new Store(1, "Me", "Home", "000000000000", items);
+	final ItemDTO itemDTO = new ItemDTO(1, "Instrument", "String", "Guitar", "Fender", "Classic", 1000, 10, store.getId());
+	final List<ItemDTO> itemDTOs = new ArrayList<>(Arrays.asList(itemDTO));
+	final StoreDTO storeDTO = new StoreDTO(1, "Me", "Home", "000000000000", itemDTOs);
+	final List<Store> stores = new ArrayList<>(Arrays.asList(store));
+	final List<StoreDTO> storeDTOs = new ArrayList<>(Arrays.asList(storeDTO));
 
 	@Autowired
 	private StoreServiceDB service;
@@ -30,23 +40,16 @@ class StoreServiceUnitTest {
 
 	@Test
 	void mapToDTOTest() {
-		final Store store = new Store(1, "Me", "Home", "000000000000");
-		final StoreDTO storeDTO = new StoreDTO(1, "Me", "Home", "000000000000");
 		assertEquals(storeDTO, service.mapToDTO(store));
 	}
 
 	@Test
 	void mapListToDTOTest() {
-		final List<Store> stores = new ArrayList<>(Arrays.asList(new Store(1, "Me", "Home", "000000000000")));
-		final List<StoreDTO> storeDTOs = new ArrayList<>(Arrays.asList(new StoreDTO(1, "Me", "Home", "000000000000")));
 		assertEquals(storeDTOs, service.mapToDTO(stores));
 	}
 
 	@Test
 	void testCreate() {
-		final Store store = new Store(1, "Me", "Home", "000000000000");
-		final StoreDTO storeDTO = new StoreDTO(1, "Me", "Home", "000000000000");
-
 		Mockito.when(repo.save(store)).thenReturn(store);
 
 		assertEquals(storeDTO, service.create(store));
